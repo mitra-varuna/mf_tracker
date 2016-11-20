614f583ef17c4e17423c70e9964287f1c3d364ab
Sushant <sushant.srivastav@gmail.com>
2016-11-20 14:43:13 +0530

    Added documentation

--- models.go
+++ models.go
@@ -5,7 +5,7 @@ import (
 	"time"
 )
 
-
+//A Mutual fund model
 type MutualFund struct {
 	ISIN string
 	CurrentNAV big.Float
@@ -13,20 +13,24 @@ type MutualFund struct {
 	SalePrice big.Float
 }
 
+//A mutual fund investment model
 type MutualFundInvestment struct {
 	Fund MutualFund
 	Units float32
 	InvestmentDate time.Time
 }
 
+//Portfolio holds a number of mutual funds
 type PortFolio struct {
 	Investments []MutualFundInvestment
 }
 
+//Get Current value of the mutual fund investment
 func (m MutualFundInvestment) CurrentValue() big.Float{
 	return m.Fund.CurrentNAV * m.Units
 }
 
+//Get Total Value of the Investments
 func (p PortFolio) TotalValue() big.Float{
 	sum := 0
 	for _, investment := range p.Investments {
@@ -35,6 +39,7 @@ func (p PortFolio) TotalValue() big.Float{
 	return sum
 }
 
+//Add a new mutual fund investment to the portfolio
 func (p PortFolio) NewInvestment(m MutualFund, amount big.Float) {
 	var units = amount/m.CurrentNAV
 	investment := MutualFundInvestment{Fund:m, Units:units, InvestmentDate:time.Now()}
