cc494d82b83d66dcd897efe094a176a1490e9254
Sushant <sushant.srivastav@gmail.com>
2017-08-13 12:45:52 +0530

    Docs

--- models.go
+++ models.go
@@ -1,36 +1,36 @@
-package mf_tracker
+package mftracker
 
 import (
 	"time"
 )
 
-//A Mutual fund model
+//MutualFund model
 type MutualFund struct {
-	ISIN string
-	CurrentNAV float64
+	ISIN            string
+	CurrentNAV      float64
 	RePurchasePrice float64
-	SalePrice float64
+	SalePrice       float64
 }
 
-//A mutual fund investment model
+//MutualFundInvestment model
 type MutualFundInvestment struct {
-	Fund MutualFund
-	Units float64
+	Fund           MutualFund
+	Units          float64
 	InvestmentDate time.Time
 }
 
-//Portfolio holds a number of mutual funds
+//PortFolio holds a number of mutual funds
 type PortFolio struct {
 	Investments []MutualFundInvestment
 }
 
 //Get Current value of the mutual fund investment
-func (m MutualFundInvestment) CurrentValue() float64{
+func (m MutualFundInvestment) CurrentValue() float64 {
 	return m.Fund.CurrentNAV * m.Units
 }
 
 //Get Total Value of the Investments
-func (p PortFolio) TotalValue() float64{
+func (p PortFolio) TotalValue() float64 {
 	sum := 0.0
 	for _, investment := range p.Investments {
 		sum += investment.CurrentValue()
@@ -40,7 +40,8 @@ func (p PortFolio) TotalValue() float64{
 
 //Add a new mutual fund investment to the portfolio
 func (p *PortFolio) NewInvestment(m MutualFund, amount float64) {
-	var units = amount/m.CurrentNAV
-	investment := MutualFundInvestment{Fund:m, Units:units, InvestmentDate:time.Now()}
+	var units = amount / m.CurrentNAV
+
+	investment := MutualFundInvestment{Fund: m, Units: units, InvestmentDate: time.Now()}
 	p.Investments = append(p.Investments, investment)
 }
